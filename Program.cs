d4da4781a7cdc764a70ccf0a84164561846a0d8a
Fotis Kyritsakis <fotiskiritsakis@gmail.com>
2026-08-05 11:14:13 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+Builder
+    .GetBuilder()
+    .SetWindowSizeInCells(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<RogueHunters.Scenes.RootScreen>()
+    .IsStartingScreenFocused(true)
+    .ConfigureFonts(true)
+    .Run();
