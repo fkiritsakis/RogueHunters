f633b9145211da10b57bcc37fd9f20548631289d
Fotis Kyritsakis <fotiskiritsakis@gmail.com>
2026-08-10 16:57:19 +0300

    Created a monster entry class and a json that will hold monster glossary entries

---
+++ MonsterEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueHunters
+{
+    class MonsterEntry
+    {
+        public string Name; //Monster Name
+        public string Description; //Visual Description of the Monster
+        public string Lore; // Lore from onlnine sources etc to make it seem more real
+        public string[] Weaknesses; // Weaknesses to silver, holy water, blessed weapons, iron
+        public string Mythology; //Mythology of Origin
+        public enum ThreatLevel; // High, Mid, Low
+
+
+        public MonsterEntry(string name, string description) 
+        {
+            name = this.Name;
+            description = this.Description;
+        }
+    }
+
+
+}
