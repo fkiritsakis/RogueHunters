05539ba01438b7b8d84dc08eb6412fc3dba45fa7
Fotis Kyritsakis <fotiskiritsakis@gmail.com>
2026-08-11 12:57:04 +0300

    Monster Glossary (Monstrum) related upadtes
    Filled the MonsterDatabase class with a loadMonsters method to read from json file. Created a MonstrumScreen class that inherites from ScreenSurface, similar to root screen to display the rexpaint placeholder screen and debug/try out how to display the data of each monster/enemy

--- MonsterDatabase.cs
+++ MonsterDatabase.cs
@@ -2,11 +2,39 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace RogueHunters
 {
-    class MonsterDatabase 
+    public class MonsterDatabase 
     {
+        private MonsterEntry[] _monsters = Array.Empty<MonsterEntry>();
+
+        public MonsterEntry[] Monsters => _monsters;
         public MonsterDatabase() { }
+
+
+        /// <summary>
+        /// Reads monster data from a json file and populates array
+        /// </summary>
+        /// <param name="filePath">Path to Json File</param>
+        public void LoadMonsters(string filePath) 
+        {
+            if (!File.Exists(filePath)) 
+            {
+                throw new FileNotFoundException("File not Found");
+            }
+
+            string jsonContent = File.ReadAllText(filePath);
+
+            var options = new JsonSerializerOptions
+            { 
+                Converters = {new JsonStringEnumConverter()}
+            };
+
+            _monsters = JsonSerializer.Deserialize<MonsterEntry[]>(jsonContent, options)
+                ?? Array.Empty<MonsterEntry>();
+
+        }
     }
 }
---
+++ MonstrumScreen.cs
@@ -0,0 +1,43 @@
+﻿namespace RogueHunters.Scenes;
+
+using SadConsole;
+using SadConsole.UI;
+using SadConsole.UI.Controls;
+using SadRogue.Primitives;
+using System;
+
+class MonstrumScreen : ScreenObject
+{
+    private ScreenSurface _mainSurface;
+    private ControlsConsole _uiConsole;
+    private String _tittleScreenPath = "Assets/Screens/MonsterGlossaryScreen.xp";
+
+
+    //Button Settings //tbd add this to game settings maybe
+    public int btnWidth = 18;
+    public int btnHeight = 1;
+    public int centerX;
+
+    public MonstrumScreen()
+    {
+
+        centerX = (GameSettings.GAME_WIDTH - btnWidth) / 2;
+
+        //Get the rexpaint image and save it as a variable
+        var rexDocument = SadConsole.Readers.REXPaintImage.Load(System.IO.File.OpenRead(_tittleScreenPath));
+
+        var surface = rexDocument.ToCellSurface();
+        var baseLayer = surface[0];
+
+        // Create a surface that's the same size as the screen.
+        _mainSurface = new ScreenSurface(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT);
+
+        baseLayer.Copy(_mainSurface.Surface);
+
+        _uiConsole = new ControlsConsole(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT);
+
+        _uiConsole.UseKeyboard = true;
+        _uiConsole.UseMouse = true;
+
+    }
+}
