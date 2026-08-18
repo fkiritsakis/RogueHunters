f6d062605286038eab0bf6197a70ffbcd63aa7ed
Fotis Kyritsakis <fotiskiritsakis@gmail.com>
2026-08-18 22:25:50 +0300

    Create Hunter.cs
    Started working on the Hunter class to move to character creation. Just to some point where I can hardcode a hunter and start working on combat and enemy behavior before going back to actual character creation

---
+++ Hunter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueHunters
+{
+    public class Hunter
+    {
+        string Name;
+
+        /// <summary>
+        /// All four skills have effects and rolls both in Investigation phase and in
+        /// Hunting phase. 
+        /// 
+        /// Observations:
+        ///     Investigation: Spotting hidden environmental details
+        ///     Combat: Determines if you notice an ambush before combat starts, granting
+        ///     a Free Strike.
+        /// 
+        /// Lore/Erudition:
+        ///     Investigation: Accurately identify what monster you are tracking based on
+        ///     Rumors, or clues. Higher Lore reveals more explicit weaknesses ("This is a
+        ///      Strigoi, Vulnerable to Garlic and Iron")
+        ///      Combat: Unlocks bonus damage or critical multipliers when exploiting correct
+        ///      weaknesses. Using the wrong weapon, this stat won't save you.
+        /// 
+        /// Grit/Nerves:
+        ///     Investigation: Resisting panic or madness when coming face-to-face with horrifying
+        ///     scenes.
+        ///     Combat: Paranormal entities project fear, illusions or psychic dread. Low Nerves
+        ///     causes your attacks to miss, your hand to shake while loading or your character to
+        ///     freeze if not outright fly in a fit of sheer panic.
+        ///     
+        ///     
+        /// Instinct/Reflexes:
+        ///     Investigation: Tailing Moving targets without beign spotted, quick maneuvers,
+        ///     escaping collapsing investigation sites.
+        ///     Combat: Dictates Initiative (who attacks first) and your ability to Dodge or 
+        ///     Parry a lethat Strike. In brutal combat, hitting first with a prepared weakness
+        ///     ends the fight immediately.
+        /// </summary>
+        //Skills
+        int Observation;
+
+        int Lore;
+
+        int Nerves;
+
+        int Instinct;
+
+        //Stats
+
+
+        public Hunter(string hunterName, int hunterObservation, int hunterLore, int hunterNerves, int hunterInstinct) 
+        {
+            this.Name = hunterName;
+            this.Observation = hunterObservation;
+            this.Lore = hunterLore;
+            this.Nerves = hunterNerves;
+            this.Instinct = hunterInstinct;
+        }
+    }
+}
