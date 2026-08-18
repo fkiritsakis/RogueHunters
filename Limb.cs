728875317eb92cc5de2cc94e114ebd6e029f22f6
Fotis Kyritsakis <fotiskiritsakis@gmail.com>
2026-08-18 22:43:34 +0300

    Create Limb.cs
    Created a Limb class that will work with the Hunter class for now so that the player has to take care of five Limbs instead of just a flat HP number.

---
+++ Limb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueHunters
+{
+
+    public enum LimbState 
+    {
+        Healthy,
+        Bleeding,
+        Burned,
+        Broken,
+        Shattered,
+        Severed
+    }
+
+    public class Limb
+    {
+        public string Name;
+        public LimbState LimbState;
+
+        public Limb(string name, LimbState limbState) 
+        {
+            this.Name = name;
+            this.LimbState = limbState;
+        }
+
+        public void SetLimbState(LimbState limbState) 
+        {
+            this.LimbState = limbState;
+        }
+
+        public LimbState GetLimbState() 
+        {
+            return this.LimbState;
+        }
+        
+    }
+}
